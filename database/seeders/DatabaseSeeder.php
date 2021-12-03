6046fb511caad997dc6cb18dced309131ae94459
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-12-03 13:06:05 +0300

    Create a seeder for categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,19 +2,24 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-        User::factory(10)->create();
-    }
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		// \App\Models\User::factory(10)->create();
+		for ($i = 0; $i < 10; $i++) {
+			DB::table('categories')->insert([
+				'name' => Str::random(10),
+			]);
+		}
+	}
 }
