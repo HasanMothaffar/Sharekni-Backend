a24d9966c451998cfbcd3d76ca18d60ed026f0d2
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-12-04 17:09:00 +0300

    Add basic product filtration, sorting, and pagination

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,62 +2,135 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index(Request $request)
+	{
+		// First, filter by these fields (if available): Category, product name, expiration date
+		// Then sort by name, price, expiration date (asc, desc)
+		// Then return paginated results
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+		$category_id = $request->query('category');
+		$name = $request->query('name');
+		$sort = $request->query('sort');
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        
-    }
+		$products =
+			Product::when($category_id, function ($query, $category_id) {
+				return $query->where('category_id', $category_id);
+			})
+			->when($name, function ($query, $name) {
+				return $query->where('name', 'like', "%$name%");
+			})
+			->when($sort, function ($query, $sort) {
+				// Example request: ?sort=name_asc
+				// Example 2 request: ?sort=price_desc
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+				$sort_fields = explode('_', $sort);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+				$column = $sort_fields[0];
+				$direction = $sort_fields[1];
+
+				$valid_sort_columns = ['name', 'price', 'expiry_date'];
+				if (!in_array($column, $valid_sort_columns)) return $query;
+
+				return $query->orderBy($column, $direction);
+			})->paginate(6);
+
+		return response()->json([
+			'data' => $products->items(),
+			'current_page' => $products->currentPage(),
+			'per_page' => $products->perPage(),
+			'last_page' => $products->lastPage()
+		], 200);
+
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$request->validate([
+			'name' => 'required',
+			'price' => 'required',
+			'price_1' => 'required',
+			'price_2' => 'required',
+			'price_3' => 'required',
+			'description' => 'required',
+			'expiry_date' => 'required',
+			'img_url' => 'required',
+			'quantity' => 'required',
+			'category_id' => 'required',
+			'owner_id' => 'required'
+		]);
+		return Product::create($request->all());
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		try {
+			$product = Product::findOrFail($id);
+			$product->views += 1;
+			$product->save();
+
+			return response()->json(['data' => $product], 200);
+		} catch (ModelNotFoundException $e) {
+			return response()->json(['message' => 'Product not found.'], 404);
+		}
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$product = Product::find($id);
+
+		if (!$product) {
+			return response()->json(['message' => 'Product not found.'], 404);
+		}
+
+		// $product->newField = $request->newField;
+		// $product->save();
+		// return response()->json(['message' => 'Product updated successfully!', 'data' => $product], 200);
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		try {
+			$product = Product::findOrFail($id);
+			$product->delete();
+			return response()->json(['message' => 'Product deleted successfully!'], 200);
+		} catch (ModelNotFoundException $e) {
+			return response()->json(['message' => 'Product not found.'], 404);
+		}
+	}
 }
