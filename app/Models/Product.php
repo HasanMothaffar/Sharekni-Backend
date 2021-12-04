3c3f77c7fba043f859dd2bdda2159275e7da8488
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-12-04 19:41:45 +0300

    Adjust the hidden and fillable properties of Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
 	use HasFactory;
+
+	protected $fillable = [
+		'name',
+		'original_price',
+		'price_1',
+		'price_2',
+		'price_3',
+		'description',
+		'expiry_date',
+		'img_url',
+		'quantity',
+		'category_id',
+	];
+
+	protected $hidden = [
+		'owner_id'
+	];
 }
