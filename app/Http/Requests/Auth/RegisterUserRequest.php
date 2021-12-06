6bd7c4674cb2a8b7c0dd69382fb71b0ca36e887e
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-12-06 17:20:56 +0300

    Split validation logic for login/registration requests

---
+++ app/Http/Requests/Auth/RegisterUserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		/**
+		 * Note that clients have to send a 'password_confirmation' field
+		 */
+		return [
+			'name' => 'required|string|max:255',
+			'email' => 'required|string|email|unique:users,email',
+			'password' => 'required|string|min:6|confirmed'
+		];
+	}
+}
