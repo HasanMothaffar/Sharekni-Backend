5fb795f9b502a0a8382163cc518d830cb8e1fdde
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-12-06 20:28:28 +0300

    Create a basic reviews controller

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class ReviewsController extends Controller
 {
-    //
+    public function index (Request $request, $id) {
+		$sort = $request->query('sort');
+		$rating = $request->query('rating');
+
+		$reviews =
+			Review::where('product_id', $id)
+			->when($rating, function ($query, $rating) {
+				return $query->where('rating', $rating);
+			})
+			->when($sort, function ($query, $sort) {
+				// TODO: Sort by date and rating
+
+				$direction = request()->query('dir', 'asc');
+
+				$valid_sort_columns = ['rating', 'date'];
+				$valid_directions = ['asc', 'desc'];
+
+
+				return $query->orderBy($sort_column, $direction);
+			})
+			->paginate(6);
+
+		return $reviews;
+	}
+
+	public function destroy($id) {
+		try {
+			$review = Review::findOrFail($id);
+			$review->destroy();
+			return response()->json(['message' => 'Review deleted successfully!'], 200);
+		} catch (ModelNotFoundException $e) {
+			return response()->json(['message' => 'Review not found.'], 404);
+		}
+	}
 }
