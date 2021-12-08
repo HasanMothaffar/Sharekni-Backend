825832d1624a5948d2bd6fdb378ed26e347d0bef
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-12-08 14:12:30 +0300

    Hash the user's password in UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -21,7 +22,7 @@ class UserSeeder extends Seeder
 		User::create([
 			'name' => 'Hasan',
 			'email' => 'hasan.mozafar@gmail.com',
-			'password' => '12345678'
+			'password' => bcrypt('12345678')
 		]);
 	}
 }
