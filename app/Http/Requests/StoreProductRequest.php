34b70311f99fb117373158631b4e21b716bee479
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-12-08 16:00:11 +0300

    Update validation rules for adding products

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -6,34 +6,34 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreProductRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
 		return [
 			'name' => 'required|string',
-			'original_price' => 'required|gt:0',
-			'price_1' => 'required|gt:0',
-			'price_2' => 'required|gt:0',
-			'price_3' => 'required|gt:0',
+			'original_price' => 'required|numeric|gt:0',
+			'price_1' => 'required|numeric|gt:0|lt:original_price',
+			'price_2' => 'required|numeric|gt:0|lt:price_1',
+			'price_3' => 'required|numeric|gt:0|lt:price_2',
 			'description' => 'required|string',
 			'expiry_date' => 'required',
 			'img_url' => 'required',
-			'quantity' => 'required|gt:0',
+			'quantity' => 'required|integer|gt:0',
 			'category_id' => 'exists:categories,id',
 		];
-    }
+	}
 }
--- database/migrations/2021_12_03_101513_create_products_table.php
+++ database/migrations/2021_12_03_101513_create_products_table.php
@@ -16,10 +16,20 @@ class CreateProductsTable extends Migration
 		Schema::create('products', function (Blueprint $table) {
 			$table->id();
 			$table->string('name');
+
+			/**
+			 * original_price: date > 30 days
+			 * price_1: date between 15 and 30
+			 * price_2: date between 7 and 15
+			 * price_3: date < 7
+			 *
+			 * where date = product_expiry_date - current_date()
+			 */
 			$table->double('original_price');
 			$table->double('price_1');
 			$table->double('price_2');
 			$table->double('price_3');
+
 			$table->integer('views')->default(0);
 			$table->longText('description');
 			$table->date('expiry_date');
