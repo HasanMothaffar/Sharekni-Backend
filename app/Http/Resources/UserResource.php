6f6ab513ec9d12aa43acd67621c11302050ad314
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-12-30 05:28:47 +0300

    Return product resources in `GET /profile`

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -6,20 +6,20 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+	 */
+	public function toArray($request)
+	{
 		return [
 			'id' => $this->id,
 			'name' => $this->name,
 			'email' => $this->email,
-			'products' => $this->products()->get(),
+			'products' => new ProductCollection($this->products()->get()),
 			'reviews' => $this->reviews()->get()
 		];
-    }
+	}
 }
