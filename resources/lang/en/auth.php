de9161ba5a8f4aea343ab0c9cd56f60971d1d26f
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-12-30 10:38:15 +0300

    Create translation files

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -2,7 +2,7 @@
 
 return [
 
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Authentication Language Lines
     |--------------------------------------------------------------------------
@@ -13,8 +13,17 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+	'failed' => 'These credentials do not match our records.',
+	'password' => 'The provided password is incorrect.',
+	'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
+	'login_success' => 'Logged in successfully!',
+	'signup_success' => 'Created an account successfully!',
+	'logout_sucess' => 'Logged out successfully!',
+
+	/**
+	 * Sent when a user is unauthorized to do an action like editing/deleting
+	 * a product that does not belong to him
+	 */
+	'unauthorized' => 'You are not authorized to do this action!',
 ];
